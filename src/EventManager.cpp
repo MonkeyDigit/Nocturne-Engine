114efab425aa4036b7b08d53a6007aa1e85a79a1
Marco Fadda <marco.faddagm@gmail.com>
2026-03-09 13:46:25 +0100

    Separated Event Manager cpp files

--- src/EventManager.cpp
+++ src/EventManager.cpp
@@ -1,36 +1,5 @@
-#include <optional>
 #include "EventManager.h"
 #include "StateManager.h"
-
-// --- TRANSLATION DICTIONARIES ---
-// Using an anonymous namespace to confine this data in this file only
-// It gets allocated in memory only once on startup
-namespace
-{
-    std::optional<EventType> ResolvePolledEventType(const sf::Event& event)
-    {
-        if (event.is<sf::Event::Closed>()) return EventType::Closed;
-        if (event.is<sf::Event::Resized>()) return EventType::Resized;
-        if (event.is<sf::Event::FocusLost>()) return EventType::FocusLost;
-        if (event.is<sf::Event::FocusGained>()) return EventType::FocusGained;
-        if (event.is<sf::Event::TextEntered>()) return EventType::TextEntered;
-        if (event.is<sf::Event::KeyPressed>()) return EventType::KeyDown;
-        if (event.is<sf::Event::KeyReleased>()) return EventType::KeyUp;
-        if (event.is<sf::Event::MouseButtonPressed>()) return EventType::MouseClick;
-        if (event.is<sf::Event::MouseButtonReleased>()) return EventType::MouseRelease;
-        if (event.is<sf::Event::MouseWheelScrolled>()) return EventType::MouseWheel;
-        return std::nullopt;
-    }
-
-    inline void MarkBindingMatched(Binding& bind, int keyCode)
-    {
-        // Store the first matched key/button code for the callback payload
-        if (bind.m_details.m_keyCode == -1)
-            bind.m_details.m_keyCode = keyCode;
-
-        ++bind.m_evCount;
-    }
-}
 
 // --- EVENT DETAILS ---
 EventDetails::EventDetails(const std::string& bindName) : m_name(bindName) { Clear(); }
@@ -52,7 +21,7 @@ void Binding::BindEvent(EventType type, int code)
     m_events.emplace_back(type, code);
 }
 
-// --- EVENT MANAGER ---
+// --- EVENT MANAGER CORE ---
 EventManager::EventManager()
     : m_currentState(StateType::Global),
     m_hasFocus(true)
@@ -62,7 +31,7 @@ EventManager::EventManager()
 
 bool EventManager::AddBinding(std::unique_ptr<Binding> binding)
 {
-    if (!binding || m_bindings.find(binding->m_name) != m_bindings.end()) 
+    if (!binding || m_bindings.find(binding->m_name) != m_bindings.end())
         return false;
 
     return m_bindings.emplace(binding->m_name, std::move(binding)).second;
@@ -85,162 +54,3 @@ bool EventManager::RemoveCallback(StateType state, const std::string& name)
 }
 
 void EventManager::SetCurrentState(StateType type) { m_currentState = type; }
-
-void EventManager::ProcessPolledEvent(const sf::Event& event)
-{
-    const std::optional<EventType> currentType = ResolvePolledEventType(event);
-    if (!currentType) return;
-
-    for (auto& [_, bindPtr] : m_bindings)
-    {
-        Binding& bind = *bindPtr;
-
-        for (const auto& [eventType, code] : bind.m_events)
-        {
-            if (eventType != *currentType) continue;
-
-            switch (eventType)
-            {
-            case EventType::KeyDown:
-            {
-                const auto* keyEvent = event.getIf<sf::Event::KeyPressed>();
-                if (keyEvent && code == static_cast<int>(keyEvent->code))
-                    MarkBindingMatched(bind, code);
-                break;
-            }
-            case EventType::KeyUp:
-            {
-                const auto* keyEvent = event.getIf<sf::Event::KeyReleased>();
-                if (keyEvent && code == static_cast<int>(keyEvent->code))
-                    MarkBindingMatched(bind, code);
-                break;
-            }
-            case EventType::MouseClick:
-            {
-                const auto* mouseEvent = event.getIf<sf::Event::MouseButtonPressed>();
-                if (mouseEvent && code == static_cast<int>(mouseEvent->button))
-                {
-                    bind.m_details.m_mouse.x = mouseEvent->position.x;
-                    bind.m_details.m_mouse.y = mouseEvent->position.y;
-                    MarkBindingMatched(bind, code);
-                }
-                break;
-            }
-            case EventType::MouseRelease:
-            {
-                const auto* mouseEvent = event.getIf<sf::Event::MouseButtonReleased>();
-                if (mouseEvent && code == static_cast<int>(mouseEvent->button))
-                {
-                    bind.m_details.m_mouse.x = mouseEvent->position.x;
-                    bind.m_details.m_mouse.y = mouseEvent->position.y;
-                    MarkBindingMatched(bind, code);
-                }
-                break;
-            }
-            case EventType::MouseWheel:
-            {
-                const auto* mouseEvent = event.getIf<sf::Event::MouseWheelScrolled>();
-                if (mouseEvent && code == static_cast<int>(mouseEvent->wheel))
-                {
-                    bind.m_details.m_mouse.x = mouseEvent->position.x;
-                    bind.m_details.m_mouse.y = mouseEvent->position.y;
-                    bind.m_details.m_mouseWheelDelta = mouseEvent->delta;
-                    MarkBindingMatched(bind, code);
-                }
-                break;
-            }
-            case EventType::Resized:
-            {
-                const auto* windowEvent = event.getIf<sf::Event::Resized>();
-                if (windowEvent)
-                {
-                    bind.m_details.m_size.x = windowEvent->size.x;
-                    bind.m_details.m_size.y = windowEvent->size.y;
-                    MarkBindingMatched(bind, code);
-                }
-                break;
-            }
-            case EventType::TextEntered:
-            {
-                const auto* textEvent = event.getIf<sf::Event::TextEntered>();
-                if (textEvent)
-                {
-                    bind.m_details.m_textEntered = textEvent->unicode;
-                    MarkBindingMatched(bind, code);
-                }
-                break;
-            }
-            case EventType::Closed:
-                ++bind.m_evCount;
-                break;
-            default:
-                ++bind.m_evCount;
-                break;
-            }
-        }
-    }
-}
-
-void EventManager::ProcessRealTimeInput()
-{
-    if (!m_hasFocus) return;
-
-    for (auto& [_, bindPtr] : m_bindings)
-    {
-        Binding& bind = *bindPtr;
-
-        for (const auto& [eventType, code] : bind.m_events)
-        {
-            switch (eventType)
-            {
-            case EventType::Keyboard:
-            case EventType::KeyboardHeld:
-                if (sf::Keyboard::isKeyPressed(static_cast<sf::Keyboard::Key>(code)))
-                    MarkBindingMatched(bind, code);
-                break;
-
-            case EventType::Mouse:
-            case EventType::MouseHeld:
-                if (sf::Mouse::isButtonPressed(static_cast<sf::Mouse::Button>(code)))
-                    MarkBindingMatched(bind, code);
-                break;
-
-            default:
-                break;
-            }
-        }
-    }
-}
-
-// At this point, if the matched events counter is equal to the number of the binding's triggering events, its callback function is called
-void EventManager::DispatchCallbacks()
-{
-    for (auto& b_itr : m_bindings)  // Iterate through the bindings
-    {
-        Binding* bind = b_itr.second.get();
-
-        if (bind->m_events.size() == bind->m_evCount)
-        {   // Using a lambda function
-            auto dispatchForState = [&](StateType state)
-                {
-                    auto callbacksIt = m_callbacks.find(state);
-                    if (callbacksIt == m_callbacks.end()) return;
-
-                    auto callItr = callbacksIt->second.find(bind->m_name);
-                    if (callItr != callbacksIt->second.end())
-                    {   // Pass in information about events
-                        callItr->second(bind->m_details);
-                    }
-                };
-
-            dispatchForState(m_currentState);
-
-            // Also dispatch global callbacks, but avoid duplicate dispatch
-            if (m_currentState != StateType::Global)
-                dispatchForState(StateType::Global);
-        }
-
-        bind->m_evCount = 0;
-        bind->m_details.Clear();
-    }
-}
---
+++ src/EventRuntime.cpp
@@ -0,0 +1,187 @@
+#include <optional>
+#include "EventManager.h"
+#include "StateManager.h"
+
+namespace
+{
+    std::optional<EventType> ResolvePolledEventType(const sf::Event& event)
+    {
+        if (event.is<sf::Event::Closed>()) return EventType::Closed;
+        if (event.is<sf::Event::Resized>()) return EventType::Resized;
+        if (event.is<sf::Event::FocusLost>()) return EventType::FocusLost;
+        if (event.is<sf::Event::FocusGained>()) return EventType::FocusGained;
+        if (event.is<sf::Event::TextEntered>()) return EventType::TextEntered;
+        if (event.is<sf::Event::KeyPressed>()) return EventType::KeyDown;
+        if (event.is<sf::Event::KeyReleased>()) return EventType::KeyUp;
+        if (event.is<sf::Event::MouseButtonPressed>()) return EventType::MouseClick;
+        if (event.is<sf::Event::MouseButtonReleased>()) return EventType::MouseRelease;
+        if (event.is<sf::Event::MouseWheelScrolled>()) return EventType::MouseWheel;
+        return std::nullopt;
+    }
+
+    inline void MarkBindingMatched(Binding& bind, int keyCode)
+    {
+        // Store the first matched key/button code for the callback payload
+        if (bind.m_details.m_keyCode == -1)
+            bind.m_details.m_keyCode = keyCode;
+
+        ++bind.m_evCount;
+    }
+}
+
+void EventManager::ProcessPolledEvent(const sf::Event& event)
+{
+    const std::optional<EventType> currentType = ResolvePolledEventType(event);
+    if (!currentType) return;
+
+    for (auto& [_, bindPtr] : m_bindings)
+    {
+        Binding& bind = *bindPtr;
+
+        for (const auto& [eventType, code] : bind.m_events)
+        {
+            if (eventType != *currentType) continue;
+
+            switch (eventType)
+            {
+            case EventType::KeyDown:
+            {
+                const auto* keyEvent = event.getIf<sf::Event::KeyPressed>();
+                if (keyEvent && code == static_cast<int>(keyEvent->code))
+                    MarkBindingMatched(bind, code);
+                break;
+            }
+            case EventType::KeyUp:
+            {
+                const auto* keyEvent = event.getIf<sf::Event::KeyReleased>();
+                if (keyEvent && code == static_cast<int>(keyEvent->code))
+                    MarkBindingMatched(bind, code);
+                break;
+            }
+            case EventType::MouseClick:
+            {
+                const auto* mouseEvent = event.getIf<sf::Event::MouseButtonPressed>();
+                if (mouseEvent && code == static_cast<int>(mouseEvent->button))
+                {
+                    bind.m_details.m_mouse.x = mouseEvent->position.x;
+                    bind.m_details.m_mouse.y = mouseEvent->position.y;
+                    MarkBindingMatched(bind, code);
+                }
+                break;
+            }
+            case EventType::MouseRelease:
+            {
+                const auto* mouseEvent = event.getIf<sf::Event::MouseButtonReleased>();
+                if (mouseEvent && code == static_cast<int>(mouseEvent->button))
+                {
+                    bind.m_details.m_mouse.x = mouseEvent->position.x;
+                    bind.m_details.m_mouse.y = mouseEvent->position.y;
+                    MarkBindingMatched(bind, code);
+                }
+                break;
+            }
+            case EventType::MouseWheel:
+            {
+                const auto* mouseEvent = event.getIf<sf::Event::MouseWheelScrolled>();
+                if (mouseEvent && code == static_cast<int>(mouseEvent->wheel))
+                {
+                    bind.m_details.m_mouse.x = mouseEvent->position.x;
+                    bind.m_details.m_mouse.y = mouseEvent->position.y;
+                    bind.m_details.m_mouseWheelDelta = mouseEvent->delta;
+                    MarkBindingMatched(bind, code);
+                }
+                break;
+            }
+            case EventType::Resized:
+            {
+                const auto* windowEvent = event.getIf<sf::Event::Resized>();
+                if (windowEvent)
+                {
+                    bind.m_details.m_size.x = windowEvent->size.x;
+                    bind.m_details.m_size.y = windowEvent->size.y;
+                    MarkBindingMatched(bind, code);
+                }
+                break;
+            }
+            case EventType::TextEntered:
+            {
+                const auto* textEvent = event.getIf<sf::Event::TextEntered>();
+                if (textEvent)
+                {
+                    bind.m_details.m_textEntered = textEvent->unicode;
+                    MarkBindingMatched(bind, code);
+                }
+                break;
+            }
+            case EventType::Closed:
+                ++bind.m_evCount;
+                break;
+            default:
+                ++bind.m_evCount;
+                break;
+            }
+        }
+    }
+}
+
+void EventManager::ProcessRealTimeInput()
+{
+    if (!m_hasFocus) return;
+
+    for (auto& [_, bindPtr] : m_bindings)
+    {
+        Binding& bind = *bindPtr;
+
+        for (const auto& [eventType, code] : bind.m_events)
+        {
+            switch (eventType)
+            {
+            case EventType::Keyboard:
+            case EventType::KeyboardHeld:
+                if (sf::Keyboard::isKeyPressed(static_cast<sf::Keyboard::Key>(code)))
+                    MarkBindingMatched(bind, code);
+                break;
+
+            case EventType::Mouse:
+            case EventType::MouseHeld:
+                if (sf::Mouse::isButtonPressed(static_cast<sf::Mouse::Button>(code)))
+                    MarkBindingMatched(bind, code);
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+}
+
+// If all events in a binding matched, dispatch its callback
+void EventManager::DispatchCallbacks()
+{
+    for (auto& b_itr : m_bindings)
+    {
+        Binding* bind = b_itr.second.get();
+
+        if (bind->m_events.size() == bind->m_evCount)
+        {
+            auto dispatchForState = [&](StateType state)
+                {
+                    auto callbacksIt = m_callbacks.find(state);
+                    if (callbacksIt == m_callbacks.end()) return;
+
+                    auto callItr = callbacksIt->second.find(bind->m_name);
+                    if (callItr != callbacksIt->second.end())
+                        callItr->second(bind->m_details);
+                };
+
+            dispatchForState(m_currentState);
+
+            // Also dispatch global callbacks, but avoid duplicate dispatch
+            if (m_currentState != StateType::Global)
+                dispatchForState(StateType::Global);
+        }
+
+        bind->m_evCount = 0;
+        bind->m_details.Clear();
+    }
+}
